28d72c9303b3facf357dfa63f27767ba5b38c9b3
Rishab Nayak <rishab@bu.edu>
2021-10-27 10:24:55 -0400

    updating rules

--- Assignments/Assignment-2/base64/base64.cpp
+++ Assignments/Assignment-2/base64/base64.cpp
@@ -5,55 +5,75 @@
 #include <vector>
 #include <cstdint>
 
-
 //hint: use CRYPT_STRING_BASE64
 
-
-std::wstring  b64Encode(std::vector<uint8_t> binaryData){
-    // note that this will convert your std::string into a c string. 
+std::wstring b64Encode(std::vector<uint8_t> binaryData)
+{
+    // note that this will convert your std::string into a c string.
     auto rawData = binaryData.data();
-    std::wstring  returnBuff;
+    std::wstring returnBuff;
     // your code here
-    // Hint: you should make two calls to ::CryptBinaryToStringW 
+    // Hint: you should make two calls to ::CryptBinaryToStringW
     // One to get the right size for the buffer
     // Then one to copy the data over
-    // std::vector<uint8_t> is a perfectly fine container for raw binary  data 
-    // as it is allocated in contiguous chunks of memory 
+    // std::vector<uint8_t> is a perfectly fine container for raw binary  data
+    // as it is allocated in contiguous chunks of memory
     // you can also easily convert it to raw data via returnBuff.data()
 
     //change me
+    DWORD buffLength = 0;
+    wchar_t *encBuff;
+    CryptBinaryToStringW(rawData, binaryData.size(), CRYPT_STRING_BASE64, NULL, &buffLength);
+    returnBuff.reserve(buffLength);
+    CryptBinaryToStringW(rawData, binaryData.size(), CRYPT_STRING_BASE64, encBuff, &buffLength);
+    returnBuff.append(encBuff);
     return returnBuff;
 }
 
-
-std::vector<uint8_t> b64Decode(std::wstring){
-    // as before you should make two calls to ::CryptStringToBinaryW 
+std::vector<uint8_t> b64Decode(std::wstring myString)
+{
+    // as before you should make two calls to ::CryptStringToBinaryW
+    DWORD binaryBuffLength = 0;
+    std::vector<uint8_t> binaryData;
+    BYTE *decBuff;
+    CryptStringToBinaryW(myString.c_str(), myString.length(), CRYPT_STRING_BASE64, NULL, &binaryBuffLength, NULL, NULL);
+    binaryData.resize(binaryBuffLength);
+    CryptStringToBinaryW(myString.c_str(), myString.length(), CRYPT_STRING_BASE64, decBuff, &binaryBuffLength, NULL, NULL);
+    for (int i = 0; i < binaryData.size(); i++)
+    {
+        binaryData[i] = decBuff[i];
+    }
+    return binaryData;
 }
 
-int wmain(int argc,  wchar_t* argv[]){
-    if(argc !=3){
+int wmain(int argc, wchar_t *argv[])
+{
+    if (argc != 3)
+    {
         std::wcout << L"Incorrect number of arguments" << std::endl;
         return 0;
     }
     std::wstring action = std::wstring(argv[1]);
-    
+
     std::wstring dataString = std::wstring(argv[2]);
 
-   
-
-    if( action == L"decode"){
+    if (action == L"decode")
+    {
         // in this case, we assume the raw data happens to also be a string
         auto resultVector = b64Decode(dataString);
         std::wstring resultStr(resultVector.begin(), resultVector.end());
-        // note needs to be none null 
+        // note needs to be none null
         std::wcout << resultStr << std::endl;
-
-    } else if( action == L"encode"){
-         // note this removes the null terminator 
+    }
+    else if (action == L"encode")
+    {
+        // note this removes the null terminator
         std::vector<uint8_t> stringData(dataString.begin(), dataString.end());
 
-        std::wcout << b64Encode(stringData ) << std::endl;
-    } else{
+        std::wcout << b64Encode(stringData) << std::endl;
+    }
+    else
+    {
         std::wcout << L"Wrong action: use either decode of encode" << std::endl;
     }
     return 0;
--- Assignments/Assignment-2/http/httpClient.cpp
+++ Assignments/Assignment-2/http/httpClient.cpp
@@ -3,33 +3,116 @@
 #include <iostream>
 #include <winhttp.h>
 
+wchar_t *charToWChar(char *text)
+{
+    const size_t size = strlen(text) + 1;
+    wchar_t *wText = new wchar_t[size];
+    mbstowcs(wText, text, size);
+    return wText;
+}
 
-std::wstring makeHttpRequest(std::wstring fqdn, int port, std::wstring uri, bool useTLS){
+std::wstring makeHttpRequest(std::wstring fqdn, int port, std::wstring uri, bool useTLS)
+{
     std::wstring result;
+    WINHTTPAPI HINTERNET httpSession = WinHttpOpen(L"501_Client", WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
+    WINHTTPAPI HINTERNET httpConnection = WinHttpConnect(httpSession, fqdn.c_str(), port, 0);
+    unsigned long tlsFlag;
+    if (useTLS)
+    {
+        tlsFlag = WINHTTP_FLAG_SECURE;
+    }
+    else
+    {
+        tlsFlag = 0;
+    }
+    WINHTTPAPI HINTERNET httpRequest = WinHttpOpenRequest(httpConnection, L"GET", NULL, uri.c_str(), WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, tlsFlag);
+    DWORD dwflags = SECURITY_FLAG_IGNORE_UNKNOWN_CA | SECURITY_FLAG_IGNORE_CERT_DATE_INVALID | SECURITY_FLAG_IGNORE_CERT_CN_INVALID | SECURITY_FLAG_IGNORE_CERT_WRONG_USAGE;
+    BOOL opt = WinHttpSetOption(httpRequest, WINHTTP_OPTION_SECURITY_FLAGS, &dwflags, sizeof(dwflags));
+    BOOL req = WinHttpSendRequest(httpRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0);
+    if (!req)
+    {
+        printf("%u\n", GetLastError());
+        result.append(L"Request Send Failure");
+        WinHttpCloseHandle(httpSession);
+        WinHttpCloseHandle(httpConnection);
+        WinHttpCloseHandle(httpRequest);
+        return result;
+    }
+    BOOL res = WinHttpReceiveResponse(httpRequest, NULL);
+    if (!res)
+    {
+        printf("%u\n", GetLastError());
+        result.append(L"Request Response Failure");
+        WinHttpCloseHandle(httpSession);
+        WinHttpCloseHandle(httpConnection);
+        WinHttpCloseHandle(httpRequest);
+        return result;
+    }
+
+    DWORD dwSize = 0;
+    DWORD dwDownloaded = 0;
+    char *pszOutBuffer;
+    do
+    {
+        dwSize = 0;
+        if (!WinHttpQueryDataAvailable(httpRequest, &dwSize))
+        {
+            printf("%u\n", GetLastError());
+            result.append(L"WinHttpQueryDataAvailable Error");
+        }
+        pszOutBuffer = new char[dwSize + 1];
+        if (!pszOutBuffer)
+        {
+            printf("OOM Error\n");
+            dwSize = 0;
+        }
+        else
+        {
+            ZeroMemory(pszOutBuffer, dwSize + 1);
+            if (!WinHttpReadData(httpRequest, (LPVOID)pszOutBuffer, dwSize, &dwDownloaded))
+            {
+                printf("%u\n", GetLastError());
+                result.append(L"WinHttpReadData Error");
+            }
+            else
+            {
+                result.append(charToWChar(pszOutBuffer));
+            }
+            delete[] pszOutBuffer;
+        }
+    } while (dwSize > 0);
+    WinHttpCloseHandle(httpSession);
+    WinHttpCloseHandle(httpConnection);
+    WinHttpCloseHandle(httpRequest);
     return result;
 }
 
-int wmain(int argc,  wchar_t* argv[]){
-    if(argc !=5){
+int wmain(int argc, wchar_t *argv[])
+{
+    if (argc != 5)
+    {
         std::wcout << L"Incorrect number of arguments: you need 4 positional arguemts" << std::endl;
         return 0;
     }
 
     std::wstring fqdn = std::wstring(argv[1]);
-    int port = std::stoi( argv[2] );
+    int port = std::stoi(argv[2]);
     std::wstring uri = std::wstring(argv[3]);
-    int  useTLS =std::stoi(argv[4]);
+    int useTLS = std::stoi(argv[4]);
     bool tls;
-    if (useTLS == 1){
+    if (useTLS == 1)
+    {
         tls = true;
-    } else if (useTLS == 0){
+    }
+    else if (useTLS == 0)
+    {
         tls = false;
-
-    } else{
+    }
+    else
+    {
         std::wcout << L"bad value for useTls" << std::endl;
         return 0;
     }
-     std::wcout << makeHttpRequest(fqdn,  port, uri, tls) << std::endl;
+    std::wcout << makeHttpRequest(fqdn, port, uri, tls) << std::endl;
     return 0;
-    
 }
